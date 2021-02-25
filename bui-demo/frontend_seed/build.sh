08c689372f6c1c1a410e8a38b48a7366f8dedc63
Andrew Straw <strawman@astraw.com>
2021-02-25 08:24:28 +0100

    remove mistaken comment

--- bui-demo/frontend_seed/build.sh
+++ bui-demo/frontend_seed/build.sh
@@ -8,7 +8,3 @@ wasm-pack build --target web
 
 # Copy our static stuff and put it in `pkg/`, too.
 cp static/* pkg/
-
-# above built everything, let's now run it locally
-echo "Build OK. Now run with:\n"
-echo "    microserver --port 8000 --no-spa pkg"
