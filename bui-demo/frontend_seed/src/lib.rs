55a25ef552edf6574cae074218f6cacd40e681b9
Andrew Straw <strawman@astraw.com>
2022-09-29 09:51:52 +0200

    demo: update seed backend

--- bui-demo/frontend_seed/src/lib.rs
+++ bui-demo/frontend_seed/src/lib.rs
@@ -1,8 +1,5 @@
-#[macro_use]
-extern crate seed;
-use futures::Future;
-use seed::fetch;
 use seed::prelude::*;
+use wasm_bindgen::closure::Closure;
 
 use wasm_bindgen::JsCast;
 use web_sys::{EventSource, MessageEvent};
@@ -10,6 +7,28 @@ use web_sys::{EventSource, MessageEvent};
 use bui_demo_data::{Callback, Shared};
 
 const ENTER_KEY: u32 = 13;
+
+// -----------------------------------------------------------------------------
+
+// Init
+
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    let events_url = "events";
+    let es = EventSource::new(events_url).unwrap();
+    let connection_state = es.ready_state();
+    let shared = None;
+
+    register_es_handler("open", Msg::Connected, &es, orders);
+    register_es_handler("bui_backend", Msg::ServerMessage, &es, orders);
+    register_es_handler("error", Msg::Error, &es, orders);
+
+    Model {
+        shared,
+        es,
+        local_name: "".to_string(),
+        connection_state,
+    }
+}
 
 // -----------------------------------------------------------------------------
 
@@ -35,7 +54,7 @@ pub enum Msg {
     UpdateName(String),
     EditKeyDown(u32), // keycode
     ToggleRecording,
-    Fetched(fetch::ResponseDataResult<()>),
+    Fetched(Result<(), String>),
 }
 
 fn update(msg: Msg, mut model: &mut Model, orders: &mut impl Orders<Msg>) {
@@ -47,7 +66,7 @@ fn update(msg: Msg, mut model: &mut Model, orders: &mut impl Orders<Msg>) {
                 Ok(data_result) => {
                     model.shared = Some(data_result);
                 }
-                Err(e) => error!("error in response", e),
+                Err(e) => seed::error!("error in response", e),
             }
         }
         Msg::Connected(_) => {
@@ -60,7 +79,7 @@ fn update(msg: Msg, mut model: &mut Model, orders: &mut impl Orders<Msg>) {
             let name = model.local_name.clone();
             orders
                 .skip()
-                .perform_cmd(send_message(&Callback::SetName(name)));
+                .perform_cmd(async { send_message(&Callback::SetName(name)).await });
         }
         Msg::UpdateName(local_name) => {
             model.local_name = local_name;
@@ -76,26 +95,38 @@ fn update(msg: Msg, mut model: &mut Model, orders: &mut impl Orders<Msg>) {
             } else {
                 false
             };
-            orders
-                .skip()
-                .perform_cmd(send_message(&Callback::SetIsRecording(new_value)));
+            orders.skip().perform_cmd(async move {
+                send_message(&Callback::SetIsRecording(new_value)).await
+            });
         }
         Msg::Fetched(Ok(_response_data)) => {
             // fetch successful
         }
         Msg::Fetched(Err(fail_reason)) => {
-            error!("callback fetch error:", fail_reason);
+            seed::error!("callback fetch error:", fail_reason);
             orders.skip();
         }
     }
 }
 
-fn send_message(payload: &Callback) -> impl Future<Output = Result<Msg, Msg>> {
+async fn send_message(payload: &Callback) -> Msg {
     let url = "callback";
-    fetch::Request::new(url)
-        .method(fetch::Method::Post)
-        .send_json(payload)
-        .fetch_json_data(Msg::Fetched)
+
+    let request = Request::new(url)
+        .method(Method::Post)
+        .json(&payload)
+        .expect("Serialization failed");
+
+    let response = fetch(request).await.expect("HTTP request failed");
+
+    let result = response
+        .check_status() // ensure we've got 2xx status
+        .expect("status check failed")
+        .json::<()>()
+        .await
+        .map_err(|e| format!("{:?}", e));
+
+    Msg::Fetched(result)
 }
 
 // -----------------------------------------------------------------------------
@@ -103,17 +134,18 @@ fn send_message(payload: &Callback) -> impl Future<Output = Result<Msg, Msg>> {
 // View
 
 fn view(model: &Model) -> Node<Msg> {
+    use seed::*;
     div![
         h3!["BUI - Rust Backend, Seed Frontend - Demo"],
         p![button![
-            simple_ev(Ev::Click, Msg::ToggleRecording),
+            ev(Ev::Click, |_| Msg::ToggleRecording),
             "Toggle Recording"
         ],],
         p![label![
             "Name ",
             input![
                 attrs! {At::Value => &model.local_name},
-                simple_ev(Ev::Blur, Msg::SendName),
+                ev(Ev::Blur, |_| Msg::SendName),
                 input_ev(Ev::Input, Msg::UpdateName),
                 keyboard_ev(Ev::KeyDown, move |ev| Msg::EditKeyDown(ev.key_code())),
             ],
@@ -125,24 +157,6 @@ fn view(model: &Model) -> Node<Msg> {
 
 // -----------------------------------------------------------------------------
 
-fn after_mount(_: Url, orders: &mut impl Orders<Msg>) -> AfterMount<Model> {
-    let events_url = "events";
-    let es = EventSource::new(events_url).unwrap();
-    let connection_state = es.ready_state();
-    let shared = None;
-
-    register_es_handler("open", Msg::Connected, &es, orders);
-    register_es_handler("bui_backend", Msg::ServerMessage, &es, orders);
-    register_es_handler("error", Msg::Error, &es, orders);
-
-    AfterMount::new(Model {
-        shared,
-        es,
-        local_name: "".to_string(),
-        connection_state,
-    })
-}
-
 fn register_es_handler<T, F>(type_: &str, msg: F, es: &EventSource, orders: &mut impl Orders<Msg>)
 where
     T: wasm_bindgen::convert::FromWasmAbi + 'static,
@@ -150,7 +164,7 @@ where
 {
     let (app, msg_mapper) = (orders.clone_app(), orders.msg_mapper());
 
-    let closure = Closure::new(move |data| {
+    let closure: Closure<dyn FnMut(T)> = Closure::new(move |data| {
         app.update(msg_mapper(msg(data)));
     });
 
@@ -160,8 +174,7 @@ where
 }
 
 #[wasm_bindgen(start)]
-pub fn render() {
-    seed::App::builder(update, view)
-        .after_mount(after_mount)
-        .build_and_start();
+pub fn start() {
+    // Mount the `app` to the element with the `id` "app".
+    App::start("app", init, update, view);
 }
